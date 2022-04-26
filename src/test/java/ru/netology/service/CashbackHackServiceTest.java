50c6db2ec53cea6edf8ad923f99f101f9a1b4df2
HelgaMas <97524452+HelgaMas@users.noreply.github.com>
2022-04-26 17:06:51 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -35,4 +35,10 @@ public class CashbackHackServiceTest {
         int expected = 800;
         assertEquals(expected,service.remain(amount));
     }
+    @Test
+    public void shouldCountRemainsWithError() {
+        int amount = 1000;
+        int expected = 0;
+        assertEquals(expected,service.remain(amount));
+    }
 }
